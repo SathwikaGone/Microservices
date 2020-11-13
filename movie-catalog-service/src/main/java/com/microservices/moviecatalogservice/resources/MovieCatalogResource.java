c75ee8e1c89cc21017328289ea6987cade4ce863
SathwikaGone <gonesathwika@gmail.com>
2020-11-13 15:46:32 -0500

    Hard coded ratings in movie catalog services

--- movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,11 +2,16 @@ package com.microservices.moviecatalogservice.resources;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.io.Console;
+import java.util.Arrays;
+
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.microservices.moviecatalogservice.models.CatalogItem;
+import com.microservices.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +20,20 @@ public class MovieCatalogResource {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 		
-		return Collections.singletonList(
-				 new CatalogItem( "Athadu", "Test", 4));
+		//get all rated movieids
+		List<Rating> ratingsList= Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("1235", 3),
+				new Rating("1236", 2),
+				new Rating("1237", 5)
+				);
+		
+		// for each movieid, call movie info service and get details
+	return	ratingsList.stream().map(rating -> new CatalogItem( "Athadu", "Test", rating.getRating())).collect(Collectors.toList());
+
+		//put them all together
+//		return Collections.singletonList(
+//				 new CatalogItem( "Athadu", "Test", 4));
 	}
 
 }
