45f17a681a2018f0942435eea7f54348d39d1806
SathwikaGone <gonesathwika@gmail.com>
2020-11-13 16:26:10 -0500

    calling movie api using rest template

--- movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,16 +1,17 @@
 package com.microservices.moviecatalogservice.resources;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.io.Console;
 import java.util.Arrays;
 
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import com.microservices.moviecatalogservice.models.CatalogItem;
+import com.microservices.moviecatalogservice.models.Movie;
 import com.microservices.moviecatalogservice.models.Rating;
 
 @RestController
@@ -20,7 +21,9 @@ public class MovieCatalogResource {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 		
-		//get all rated movieids
+		RestTemplate restTemplate = new RestTemplate();
+		
+		//get all rated movieIds
 		List<Rating> ratingsList= Arrays.asList(
 				new Rating("1234", 4),
 				new Rating("1235", 3),
@@ -28,12 +31,21 @@ public class MovieCatalogResource {
 				new Rating("1237", 5)
 				);
 		
-		// for each movieid, call movie info service and get details
-	return	ratingsList.stream().map(rating -> new CatalogItem( "Athadu", "Test", rating.getRating())).collect(Collectors.toList());
+		// for each movieId, call movie info service and get details
+		
+		return	ratingsList.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId()   , Movie.class);
+			return new CatalogItem( movie.getName(), "desc", rating.getRating());
+		})
+		.collect(Collectors.toList());
 
+	
+	
+	
 		//put them all together
-//		return Collections.singletonList(
-//				 new CatalogItem( "Athadu", "Test", 4));
+	
+		//return Collections.singletonList(
+		//new CatalogItem( "Athadu", "Test", 4));
 	}
 
 }
